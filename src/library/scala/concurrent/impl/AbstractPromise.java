f3431f6130e202e4660527fe006b7ef1bc7c09ab
Viktor Klang <viktor.klang@gmail.com>
2014-03-18 18:57:17 +0100

    Makes Promise use s.c.u.Updaters instead of s.m.Unsafe

--- src/library/scala/concurrent/impl/AbstractPromise.java
+++ src/library/scala/concurrent/impl/AbstractPromise.java
@@ -9,32 +9,20 @@
 package scala.concurrent.impl;
 
 
-import scala.concurrent.util.Unsafe;
+import scala.concurrent.util.Updaters;
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
-
-
 
 abstract class AbstractPromise {
     private volatile Object _ref;
 
-    final static long _refoffset;
-
-    static {
-	try {
-	    _refoffset = Unsafe.instance.objectFieldOffset(AbstractPromise.class.getDeclaredField("_ref"));
-	} catch (Throwable t) {
-	    throw new ExceptionInInitializerError(t);
-	}
-    }
+    private final static AtomicReferenceFieldUpdater<AbstractPromise, Object> updater =
+      Updaters.newReferenceUpdater(AbstractPromise.class, Object.class, "_ref");
 
     protected final boolean updateState(Object oldState, Object newState) {
-	return Unsafe.instance.compareAndSwapObject(this, _refoffset, oldState, newState);
+	    return updater.compareAndSet(this, oldState, newState);
     }
 
     protected final Object getState() {
-	return _ref;
+	    return updater.get(this);
     }
-
-    protected final static AtomicReferenceFieldUpdater<AbstractPromise, Object> updater =
-	AtomicReferenceFieldUpdater.newUpdater(AbstractPromise.class, Object.class, "_ref");
 }
